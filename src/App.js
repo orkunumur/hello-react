f4adb4b621f57316051fa4d9bc1ad01f09328fec
orkunumur <orkunumur@gmail.com>
2023-07-01 20:17:07 +0300

    khkjhjlh

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './header'
+
+const name = "Mehmet";
+const surname= "ASDFGH";
+const isloggedin = true; 
+
 
 function App() {
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      {/* <h1>{name} {surname}</h1> */}
+      {/* <h2> {isloggedin && `Benim adim ${name}, soyadim ${surname}`}</h2>
+      <h2> {!isloggedin && `HATALI GIRIS YAPTINIZ`}</h2> */}
+      <h2> {isloggedin ? `Benim adim ${name}, soyadim ${surname}`: `HATALI GIRIS YAPTINIZ`}</h2>
+      Hello World
+
     </div>
   );
 }
---
+++ src/header.js
@@ -0,0 +1,9 @@
+function Header(){
+    return (
+        <div>
+            <h1>Header Part</h1>
+        </div>
+    );
+}
+
+export default Header ;
